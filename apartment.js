36c732bb2db3fa075d75379c908c8d034639c2a8
scorley3 <scorley3@illinois.edu>
2022-10-18 16:52:15 -0500

    formatting of filters box

--- apartment.js
+++ apartment.js
@@ -3,7 +3,7 @@ document.getElementById("closeAdv").addEventListener("click", closeAdvSearch);
 
 function openAdvSearch() {
 	document.getElementById("advancedSearchFilters").style.visibility	= "visible";
-	document.getElementById("advancedSearchFilters").style.width = "300px";
+	document.getElementById("advancedSearchFilters").style.width = "400px";
 	document.getElementById("advancedSearchFilters").style.height	= "250px";
 }
 function closeAdvSearch() {
@@ -11,3 +11,16 @@ function closeAdvSearch() {
 	document.getElementById("advancedSearchFilters").style.width = "0px";
 	document.getElementById("advancedSearchFilters").style.height	= "0px";
 }
+
+/*
+<form>
+<label class="washer">
+	<input type="checkbox" value=""> Washer/Dryer
+</label>
+</form>
+<label class="gym">
+	<input type="checkbox" value=""> Gym Access
+</label>
+</form>
+
+*/
