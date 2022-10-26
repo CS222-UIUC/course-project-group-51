7a1680e87f2c256600912cca495cfb3b16ddba87
scorley3 <scorley3@illinois.edu>
2022-10-26 14:43:49 -0500

    added skeleton of search functions to be implemented later

--- apartment.js
+++ apartment.js
@@ -1,29 +1,35 @@
+document.addEventListener("load", getAllApartments);
+function getAllApartments() {
+	//filler for now
+	//once we have json and/or csv: will be called every time page loads 
+	//will pull all of the information necessary and store the apartment data
+	//in an array or array-like object 
+}
+
 document.getElementById("advSearc").addEventListener("click",openAdvSearch);
 document.getElementById("closeAdv").addEventListener("click", closeAdvSearch);
 
+//function changes advanced search filter box to be visible and take up space
 function openAdvSearch() {
 	document.getElementById("advancedSearchFilters").style.visibility	= "visible";
 	document.getElementById("advancedSearchFilters").style.width = "400px";
 	document.getElementById("advancedSearchFilters").style.height	= "330px";
 }
+
+//function closes the advanced search filter box and shrinks it to take up no space
 function closeAdvSearch() {
 	document.getElementById("advancedSearchFilters").style.visibility	= "hidden";
 	document.getElementById("advancedSearchFilters").style.width = "0px";
 	document.getElementById("advancedSearchFilters").style.height	= "0px";
 }
 
-/*
-colors: 
-white: #fff8f6
-pink: #fff2f1
-gray: #d0cdc9
-green: #7d8743
-beige: #dabaa4
-darker beige: #c9a291
-cream: #f9f2ef
-brown: #886a5b
-orange text: #f6c197
-light purple #dec2f1
-teal: #c2ecf1
-lime green: #d4f1c2
-*/
+document.getElementById("search").addEventListener("click", search);
+document.getElementById("submitAdv").addEventListener("click", advancedSearch);
+function search() {
+	//empty for now
+	//will be implemented once there is access to data pulled from web
+}
+function advancedSearch() {
+	//same as search
+	//will also have to deal with filters 
+}
