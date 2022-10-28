5273bb65337d0ab798e822baefa4894df81ce115
maxtan-huynh <maxtan.huynh@gmail.com>
2022-10-28 15:16:54 -0500

    bankier title and address csv

--- smile_scrape.py
+++ smile_scrape.py
@@ -3,33 +3,41 @@ from lxml import html
 import requests
 import csv
 
-source = requests.get('https://www.smilestudentliving.com/availability').text
+
+# source = requests.get('https://www.smilestudentliving.com/availability').text
 # source = requests.get('https://jsmliving.com/search-available-units').text
-# source = requests.get('https://www.bankierapartments.com/apartments').text
+source = requests.get('https://www.bankierapartments.com/apartments').text
+# source = requests.get('https://ugroupcu.com/apartment-search/').text
+
 
 soup = BeautifulSoup(source, 'lxml')
 avail_units = 0
 
-csv_file = open('smile_scrape.csv', 'w')
+csv_file = open('bankier_scrape.csv', 'w')
 
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Address', 'Rent'])
+csv_writer.writerow(['Title', 'Address'])
 
-art = soup.find('div', class_ ='info').h3.text
-print(art)
+# art = soup.find('span', class_ = "title").text
+# print(art)
 
-# for article in soup.find_all('article'):
-#     print("loop")
-#     rent = article.find('div', class_ = "rent").text
-#     print(rent)
+# for article in soup.find_all('span', class_ = "address"):
+#     print(article.text)
 
+titles = []
+addresses = []
 
-# page = requests.get('https://www.smilestudentliving.com/availability')
-# tree = html.fromstring(page.content)
+for article in soup.find_all('span', class_ = "title"):
+    titles.append(article.text)
 
-# rent = tree.xpath('//*[@id="1307928463"]/div/div/div[2]/div[2]/section/div[1]/div[2]/h3/div/text()')
-# print(rent)
+for article in soup.find_all('span', class_ = "address"):
+    addresses.append(article.text)
+
+for i in range(0, len(titles)):
+    csv_writer.writerow([titles[i], addresses[i]])
 
 
 print("done")
+
+csv_file.close()
 
