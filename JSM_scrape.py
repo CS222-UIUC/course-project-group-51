6e7ff70c614f8bc1bfbac9c67c7a211a6c50ae16
maxtan-huynh <maxtan.huynh@gmail.com>
2022-11-09 11:33:15 -0600

    more pylint fixes

--- JSM_scrape.py
+++ JSM_scrape.py
@@ -8,33 +8,30 @@ source = requests.get('https://jsmliving.com/search-available-units', timeout=10
 soup = BeautifulSoup(source, 'lxml')
 AVAIL_UNITS = 0
 
-csv_file = open('JSM_scraped_info.csv', 'w', encoding='utf8')
+with open('JSM_scraped_info.csv', 'w', encoding='utf8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Address', 'Rent', 'Bedrooms', 'Bathrooms'])
 
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Address', 'Rent', 'Bedrooms', 'Bathrooms'])
+    for article in soup.find_all('article'):
+        rent = article.find('div', class_ = "unit__card-rent").text
+        if "No Units Available" in rent:
+            break
+        rent2 = rent.split(':')
 
-for article in soup.find_all('article'):
-    rent = article.find('div', class_ = "unit__card-rent").text
-    if "No Units Available" in rent:
-        break
-    rent2 = rent.split(':')
+        AVAIL_UNITS += 1
+        # print(avail_units)
+        # address = article.find('div',
+        # class_= "field field--name-field-building field--type-entity-reference
+        # field--label-hidden field--item").a.text
+        address = article.h4.a.text
+        # print(address)
 
-    AVAIL_UNITS += 1
-    # print(avail_units)
-    # address = article.find('div',
-    # class_= "field field--name-field-building field--type-entity-reference
-    # field--label-hidden field--item").a.text
-    address = article.h4.a.text
-    # print(address)
+        print(rent2[1].strip())
 
-    print(rent2[1].strip())
+        bed = article.find('div', class_ = 'unit__card-bedrooms').p.text
+        # print(bed)
 
-    bed = article.find('div', class_ = 'unit__card-bedrooms').p.text
-    # print(bed)
-
-    bath = article.find('div', class_ = 'unit__card-bathrooms').p.text
-    # print(bath)
-    # print("--------")
-    csv_writer.writerow([address, rent2[1].strip(), bed, bath])
-
-csv_file.close()
+        bath = article.find('div', class_ = 'unit__card-bathrooms').p.text
+        # print(bath)
+        # print("--------")
+        csv_writer.writerow([address, rent2[1].strip(), bed, bath])
--- bankier_scrape.py
+++ bankier_scrape.py
@@ -13,33 +13,31 @@ source = requests.get('https://www.bankierapartments.com/apartments', timeout=10
 soup = BeautifulSoup(source, 'lxml')
 AVAIL_UNITS = 0
 
-csv_file = open('bankier_scrape.csv', 'w', encoding='utf8')
+with open('bankier_scrape.csv', 'w', encoding='utf8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Company', 'Title', 'Address'])
 
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Company', 'Title', 'Address'])
+    # art = soup.find('span', class_ = "title").text
+    # print(art)
 
-# art = soup.find('span', class_ = "title").text
-# print(art)
+    # for article in soup.find_all('span', class_ = "address"):
+    #     print(article.text)
 
-# for article in soup.find_all('span', class_ = "address"):
-#     print(article.text)
+    titles = []
+    addresses = []
 
-titles = []
-addresses = []
+    for article in soup.find_all('span', class_ = "title"):
+        titles.append(article.text)
 
-for article in soup.find_all('span', class_ = "title"):
-    titles.append(article.text)
+    for article in soup.find_all('span', class_ = "address"):
+        addresses.append(article.text)
 
-for article in soup.find_all('span', class_ = "address"):
-    addresses.append(article.text)
-
-for i, title in enumerate(titles):
-    csv_writer.writerow(["Bankier", titles[i], addresses[i]])
+    for i, title in enumerate(titles):
+        csv_writer.writerow(["Bankier", title, addresses[i]])
 
 
 print("done")
 
-csv_file.close()
 
 # project_href = [i['href'] for i in soup.find_all('a', href=True)]
 # print(project_href)
